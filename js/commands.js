18ede85cf0c8c607b21e48f6736fa2c177e763e8
BillyA15 <61017717+BillyA15@users.noreply.github.com>
2023-03-13 09:31:03 +0200

    first commit

---
+++ js/commands.js
@@ -0,0 +1,90 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">[NAME] whoami</span> - [DESCRIPTION] user info',
+  '<span class="command">[NAME] su</span> - [DESCRIPTION] root user auth',
+  '<span class="command">[NAME] ls</span> - [DESCRIPTION] list directories',
+  '<span class="command">[NAME] clear</span> - [DESCRIPTION] clear terminal',
+  '<span class="command">[INPUT] ARROW_KEY_UP | ARROW KEY DOWN</span> - [DESCRIPTION] scroll through command history',
+  '<span class="command">[NAME] history</span> - [DESCRIPTION] list command history',
+  "<br>",
+];
+
+banner = [
+  "soc_vref_reg_value 0x 00000019 0000001b 00000018 0000001b 0000001a 0000001b 0000001a 0000001a 0000001b 0000001c",
+  "0000001a 0000001b 0000001b 0000001a 00000019 0000001b 0000001e 0000001b 0000001d 0000001b 0000001b 0000001c 0000001b",
+  "0000001d 0000001d 0000001c 0000001b 0000001c 0000001b 0000001c 0000001b 0000001d dram_vref_reg_value 0x 0000004f",
+"auto size-- 65535DDR cs0 size: 48MB",
+"DDR cs1 size: 0MB",
+"DMC_DDR_CTRL: 00c00054DDR size: 48MB",
+"cs0 DataBus test pass",
+"cs0 AddrBus test pass",
+"non-sec scramble use random key",
+"1STBLOB 00000200        00040000 BL2E    00040200        00013000",
+"BL2X    00053200        00011000 DDRFIP  00064200        00040000 000a4200 ",
+"100bdlr_step_size ps== 448",
+"load mask:31",
+"Load: DDRFIP From: eMMC - 3.0 src: 0000a000, dst: f700ab90, size: 00009600sdemmc src: ;",
+"Load: DDRFIP From: eMMC - 3.0 src: 00014000, dst: f700ab90, size: 00009600sdemmc src: ;",
+"Load: DDRFIP From: eMMC - 3.0 src: 0001e000, dst: f700ab90, size: 00009600sdemmc src: ;",
+"boot times 0",
+"INFO: bl2_load_bl2ex start!",
+"INFO: load ...",
+"Load: BL2E From: eMMC - 3.0 src: 00002000, dst: 00fffff0, size: 00011000sdemmc src: ;",
+"INFO: check ...",
+"SHA CHK OK!",
+];
